9031abb2acec57ebfa62dddf9920405383595707
Byounhan <hwang.byounghan@gmail.com>
2022-08-15 07:05:48 +0900

    배경이미지 경로 수정

--- js/background.js
+++ js/background.js
@@ -2,7 +2,7 @@ const bodyStyle = document.body.style;
 const images = ["0.jpeg", "1.jpeg", "2.jpeg"];
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 
-bodyStyle.backgroundImage = `url(../img/${chosenImage})`;
+bodyStyle.backgroundImage = `url(img/${chosenImage})`;
 bodyStyle.backgroundRepeat = "no-repeat";
 console.log(`url(img/${chosenImage})`);
 
